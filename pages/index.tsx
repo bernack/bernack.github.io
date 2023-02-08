acbd7f1f3aedd81a780b8d91c4524f710fc1ff5b
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-02-08 22:54:12 +0100

    all hail the css grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,100 +16,102 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.profile}>
-          <Image
-            alt="Profile picture of Jan Christoph Bernack"
-            src="/profile-jcb.jpeg"
-            width="150"
-            height="150"
-            priority
-          />
-          <div className={styles.content}>
-            <h2 className={inter.className}>
-              Jan Christoph Bernack
-            </h2>
-            <div className={styles.description}>
-              <p className={inter.className}>
-                Hamburg, Germany
-              </p>
-              <p className={inter.className}>
-                CTO @ <a href="https://www.rocknitive.com/">Rocknitive</a>
-              </p>
+        <div className={styles.profileList}>
+          <div className={styles.profile}>
+            <Image
+              alt="Profile picture of Jan Christoph Bernack"
+              src="/profile-jcb.jpeg"
+              width="150"
+              height="150"
+              priority
+            />
+            <div className={styles.content}>
+              <h2 className={inter.className}>
+                Jan Christoph Bernack
+              </h2>
+              <div className={styles.description}>
+                <p className={inter.className}>
+                  Hamburg, Germany
+                </p>
+                <p className={inter.className}>
+                  CTO @ <a href="https://www.rocknitive.com/">Rocknitive</a>
+                </p>
+              </div>
+              <ul className={styles.social}>
+                <li>
+                  <a href="https://www.linkedin.com/in/jan-christoph-bernack-248798196/"><FaLinkedinIn /></a>
+                </li>
+                {/*<li>*/}
+                {/*  <a href="mailto:jc.bernack@gmail.com"><FaMailBulk /></a>*/}
+                {/*</li>*/}
+                <li>
+                  <a href="https://github.com/jcBernack/"><FaGithub /></a>
+                </li>
+                <li>
+                  <a href="https://stackoverflow.com/users/804614/gigo"><FaStackOverflow /></a>
+                </li>
+              </ul>
             </div>
-            <ul className={styles.social}>
-              <li>
-                <a href="https://www.linkedin.com/in/jan-christoph-bernack-248798196/"><FaLinkedinIn /></a>
-              </li>
-              {/*<li>*/}
-              {/*  <a href="mailto:jc.bernack@gmail.com"><FaMailBulk /></a>*/}
-              {/*</li>*/}
-              <li>
-                <a href="https://github.com/jcBernack/"><FaGithub /></a>
-              </li>
-              <li>
-                <a href="https://stackoverflow.com/users/804614/gigo"><FaStackOverflow /></a>
-              </li>
-            </ul>
           </div>
-        </div>
-        <div className={styles.profile}>
-          <Image
-            alt="Profile picture of Timo Bernack"
-            src="/profile-tb.jpg"
-            width="150"
-            height="150"
-            priority
-          />
-          <div className={styles.content}>
-            <h2 className={inter.className}>
-              Timo Bernack
-            </h2>
-            <div className={styles.description}>
-              <p className={inter.className}>
-                Oldenburg, Germany
-              </p>
-              <p className={inter.className}>
-                Sr. Full Stack Developer @ <a href="https://www.adference.com/">Adference</a>
-              </p>
+          <div className={styles.profile}>
+            <Image
+              alt="Profile picture of Timo Bernack"
+              src="/profile-tb.jpg"
+              width="150"
+              height="150"
+              priority
+            />
+            <div className={styles.content}>
+              <h2 className={inter.className}>
+                Timo Bernack
+              </h2>
+              <div className={styles.description}>
+                <p className={inter.className}>
+                  Oldenburg, Germany
+                </p>
+                <p className={inter.className}>
+                  Sr. Full Stack Developer @ <a href="https://www.adference.com/">Adference</a>
+                </p>
+              </div>
+              <ul className={styles.social}>
+                <li>
+                  <a href="https://www.linkedin.com/in/timo-bernack-165721253/"><FaLinkedinIn /></a>
+                </li>
+                {/*<li>*/}
+                {/*  <a href="mailto:timo.bernack@bernack.de"><FaMailBulk /></a>*/}
+                {/*</li>*/}
+              </ul>
             </div>
-            <ul className={styles.social}>
-              <li>
-                <a href="https://www.linkedin.com/in/timo-bernack-165721253/"><FaLinkedinIn /></a>
-              </li>
-              {/*<li>*/}
-              {/*  <a href="mailto:timo.bernack@bernack.de"><FaMailBulk /></a>*/}
-              {/*</li>*/}
-            </ul>
           </div>
-        </div>
-        <div className={styles.profile}>
-          <Image
-            alt="Profile picture of Sina Hosbach"
-            src="/profile-sh.jpeg"
-            width="150"
-            height="150"
-            priority
-          />
-          <div className={styles.content}>
-            <h2 className={inter.className}>
-              Sina Hosbach
-            </h2>
-            <div className={styles.description}>
-              <p className={inter.className}>
-                Oldenburg, Germany
-              </p>
-              <p className={inter.className}>
-                Editor @ <a href="https://www.adference.com/">scolix-Verlag, AAP Lehrerwelt</a>
-              </p>
+          <div className={styles.profile}>
+            <Image
+              alt="Profile picture of Sina Hosbach"
+              src="/profile-sh.jpeg"
+              width="150"
+              height="150"
+              priority
+            />
+            <div className={styles.content}>
+              <h2 className={inter.className}>
+                Sina Hosbach
+              </h2>
+              <div className={styles.description}>
+                <p className={inter.className}>
+                  Oldenburg, Germany
+                </p>
+                <p className={inter.className}>
+                  Editor @ <a href="https://www.adference.com/">scolix-Verlag, AAP Lehrerwelt GmbH</a>
+                </p>
+              </div>
+              <ul className={styles.social}>
+                <li>
+                  <a href="https://www.linkedin.com/in/sina-hosbach-541ab4133/"><FaLinkedinIn /></a>
+                </li>
+                {/*<li>*/}
+                {/*  <a href="mailto:"><FaMailBulk /></a>*/}
+                {/*</li>*/}
+              </ul>
             </div>
-            <ul className={styles.social}>
-              <li>
-                <a href="https://www.linkedin.com/in/sina-hosbach-541ab4133/"><FaLinkedinIn /></a>
-              </li>
-              {/*<li>*/}
-              {/*  <a href="mailto:"><FaMailBulk /></a>*/}
-              {/*</li>*/}
-            </ul>
           </div>
         </div>
 
