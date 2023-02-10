bb04e23e774d7f7f4d03fec67a9df8393039f1e6
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-02-10 03:55:04 +0100

    remove nextjs favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <title>Bernack</title>
         <meta name="description" content="Personal site of Timo and Jan Christoph Bernack and Sina Hosbach" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        {/*<link rel="icon" href="/favicon.ico" />*/}
       </Head>
       <main className={styles.main}>
         <div className={styles.profileList}>
