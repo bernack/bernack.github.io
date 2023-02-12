d042c7831dfac37ac448a79e9a91d43516fc952a
JcBernack <JcBernack@users.noreply.github.com>
2023-02-12 02:17:16 +0000

    deploy: f4ec014a0f71330cd3f8c4cdfc5b767066658214

--- _next/static/rzD5hHoF79q5H6qREUqM4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-a250b9f78b011ef9.js","static/css/4fef18009b867cb1.css","static/chunks/pages/index-4ab4779333d69155.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
